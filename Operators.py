88361986a29d58d253cb45ff4e752126484477f4
Rimu Roy <rimuroy112@DESKTOP-Q85H7H7.localdomain>
2025-06-18 00:31:47 +0600

    Operators

---
+++ Operators.py
@@ -0,0 +1,29 @@
+# Arithmatic operators
+
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)     # Floating number
+print(5//2)    # Floor
+print(5**2)    # power
+
+# Comparison operators
+
+print(2==2) 
+print(2!=2)
+print(2>2)
+print(2<2)
+
+
+# logical operators
+
+x = 3
+y = 4
+z = 5
+print(x>y and y>x)
+print(x>y or y>x)
+
+# membership operator
+
+print('hello' in 'Hellorimu')
+print(2 in [2,3,4,5])
