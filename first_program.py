2b67831d33af704538158116dc790bf1d5ecf717
Rimu Roy <rimuroy112@DESKTOP-Q85H7H7.localdomain>
2025-06-17 23:43:29 +0600

    First program

---
+++ first_program.py
@@ -0,0 +1,12 @@
+print("welcome to the python world")
+print('xyz')
+print("xyz")
+print('''xyz''')
+print("""xyz""")
+print('''xyz
+         xyz
+         xyz''')
+name = "xyz"
+age = 21
+print(f"My name is {name} and age is {age}")     # String interpolation
+print("My name is {} and age is {}".format(name,age))  # String interpolation
