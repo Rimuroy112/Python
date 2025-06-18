48b0e0d59a78335ca53c9793846c6c107478dd47
Rimu Roy <titashroy112@gmail.com>
2025-06-19 00:18:35 +0600

    loop

--- loops.py
+++ loops.py
@@ -6,3 +6,13 @@ for fruits in my_list:
     print(fruits)
 for index,fruits in enumerate(my_list):
     print(index,fruits)
+
+for item in range(1,20,2):
+    print(item)
+
+# while
+
+count = 0
+while count < 5:
+    print('Hello')
+    count+=1
