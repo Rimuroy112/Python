34633b7d4cd9f1f549ef5865ce907a0fada75e89
Rimu Roy <titashroy112@gmail.com>
2025-06-19 00:56:02 +0600

    Functions

---
+++ Functions.py
@@ -0,0 +1,27 @@
+name = 'Rimu roy'
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# list
+my_list = [1,3,2,4]
+
+my_list.append(6)
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list.reverse()
+print(my_list)
+my_list.insert(1,5)
+print(my_list)
+my_list.remove(6)
+print(my_list)
+
+
+# User defined function
+
+def name(f_name,l_name):
+    print(f_name,l_name)
+
+name('Rimu','Roy')
